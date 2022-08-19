53c1cfcddbaf8abc8a047cf7d36951ff4a507dfa
EC2 Default User <ec2-user@ip-172-31-31-94.ec2.internal>
2022-08-19 09:41:13 +0000

    [add]ActiveStorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
