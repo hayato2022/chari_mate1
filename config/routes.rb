9bf93c9cf6a182d51632ef17eff57c1f5fb68866
EC2 Default User <ec2-user@ip-172-31-31-94.ec2.internal>
2022-08-22 13:39:15 +0000

    [add]user sign_up,sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,29 @@
 Rails.application.routes.draw do
 
+
+# ユーザー側
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
+
+
+  scope module: :public do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+  end
+
+
+
+
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
 
-  get 'homes/top'
-  get 'homes/about'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
