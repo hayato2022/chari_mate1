efa1a01929dbbfd3b86e882427bd865fd93efdd5
EC2 Default User <ec2-user@ip-172-31-31-94.ec2.internal>
2022-08-24 03:23:12 +0000

    タグ機能モデル、コントローラ記述

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,20 @@
 class Public::PostsController < ApplicationController
   def new
-    @posts = Post.new
+    @post = Post.new
   end
 
   def create
-    @posts = Post.new(post_params)
-    @posts.user_id = current_user.id
-    @posts.save
-    redirect_to post_path(@post.id)
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    # splitメソッドとは文字列を分割して配列にするメソッド
+    # 受け取った値を「,」で区切って配列にする
+    tag_list = params[:post][:name].split(',')
+    if @post.save
+      @post.save_tag(tag_list)
+      redirect_to post_path(@post.id), notice: '投稿完了しました'
+    else
+      render :new
+    end
   end
 
   def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,26 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
+
+  def save_tag(sent_tags)
+    # タグが存在していれば、タグの名前を配列として取得
+    current_tags = self.tag.pluck(:name) unless self.tags.nill?
+    # 「今あるタグ」から「新たに送られてきたタグ」を引いて、「old_tags」に代入
+    old_tags = current_tags - sent_tags
+    # 「送信されてきたタグ」から「現在存在するタグ」を除いたタグをnew_tagsとする
+    new_tags = sent_tags - current_tags
+
+    # 古いタグを消す
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(name: old)
+    end
+
+    # 新しいタグを保存
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_post_tag
+    end
+
+  end
+
 end
