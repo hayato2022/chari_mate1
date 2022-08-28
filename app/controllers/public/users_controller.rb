d6058f0ae87dce774600c43048365652e432304a
EC2 Default User <ec2-user@ip-172-31-31-94.ec2.internal>
2022-08-28 08:00:19 +0000

    [add]user/edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,21 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_customer!
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
